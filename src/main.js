dbf1bad59633c30b8fa6e074ff87463b69594a0a
haush0001 <haush0001@gmail.com>
2024-01-29 11:40:06 +0000

    working for multiple QR code generation

--- src/main.js
+++ src/main.js
@@ -1,10 +1,42 @@
+var QRCode = require("qrcode");
 
-var QRCode = require('qrcode');
+let ytChannels = [
+  "https://www.youtube.com/@codeorg",
+  "https://www.youtube.com/@CoderbyteDevelopers",
+  "https://www.youtube.com/@Codesmith",
+  "https://www.youtube.com/@craigndave",
+  "https://www.youtube.com/@decomplexify",
+  "https://www.youtube.com/@Fireship",
+  "https://www.youtube.com/@freecodecamp",
+  "https://www.youtube.com/@funfunfunction",
+  "https://www.youtube.com/@KevinPowell",
+  "https://www.youtube.com/@NetNinja",
+  "https://www.youtube.com/@TraversyMedia",
+  "https://www.youtube.com/@WesBos",
+];
 
-let qrCodeText = 'https://www.youtube.com/@codeorg';
-let filePath = 'pngfolder/myFileQR2.png';
+function generateOneQrCode(url) {
 
-QRCode.toFile(filePath, qrCodeText, function (err) {
-  if (err) throw err;
-  console.log(`QR code saved to ${filePath}`);
-});
+  let channelName = extractWordsAfterAt(url);
+  let filePath = `pngfolder/${channelName}.png`;
+
+  QRCode.toFile(filePath, url);
+}
+
+function generateAllQrCodes() {
+  for (let channel of ytChannels) {
+
+    generateOneQrCode(channel);
+  }
+}
+
+function extractWordsAfterAt(url) {
+  // Split the URL by '@' and get the second part
+  return url.split("@")[1];
+}
+
+// Apply the function to each URL in the array
+
+// console.log(extractedWords);
+
+generateAllQrCodes();
